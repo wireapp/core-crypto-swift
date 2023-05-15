242c69faad467aee7662d168d02d18abe25fb9eb
Tommaso Piazza <196761+tmspzz@users.noreply.github.com>
2023-05-15 15:56:35 +0200

    chore: restructure project

---
+++ CoreCrypto/CoreCryptoSwift/CoreCryptoSwift.h
@@ -0,0 +1,18 @@
+//
+//  CoreCryptoSwift.h
+//  CoreCryptoSwift
+//
+//  Created by Tommaso on 10.05.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreCryptoSwift.
+FOUNDATION_EXPORT double CoreCryptoSwiftVersionNumber;
+
+//! Project version string for CoreCryptoSwift.
+FOUNDATION_EXPORT const unsigned char CoreCryptoSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreCryptoSwift/PublicHeader.h>
+
+
